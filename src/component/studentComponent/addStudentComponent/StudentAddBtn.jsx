fdd654bea602aba22bf1fec553beafed18d2d1a7
Shanilka <shanilka799@gmail.com>
2024-10-20 13:40:41 +0530

    1st commit

---
+++ src/component/studentComponent/addStudentComponent/StudentAddBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StudentAddBtn() {
+    return (
+        <div>StudentAddBtn</div>
+    )
+}
+
+export default StudentAddBtn
---
+++ src/pages/studentPage/StudentPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StudentTable from '../../component/studentComponent/studentTableComponent/StudentTable'
+
+function StudentPage() {
+    return (
+        <>
+            <StudentTable />
+        </>
+    )
+}
+
+export default StudentPage
